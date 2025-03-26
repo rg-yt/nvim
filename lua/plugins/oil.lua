a7c3a6d55999c96c001c1d808b9d0a68b4d9ab51
rg-yt <103154533+rg-yt@users.noreply.github.com>
2025-03-26 11:45:10 -0400

    Update lsp-config to add html snippets to erb files

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,8 +1,13 @@
 return {
   "stevearc/oil.nvim",
+  dependencies = { { "echasnovski/mini.icons", opts = {} } },
   config = function()
     local oil = require("oil")
-    oil.setup()
+    oil.setup({
+      keymaps = {
+        ["<Esc>"] = { "actions.close", mode = "n" }, 
+      }
+    })
     vim.keymap.set("n", "-", oil.toggle_float, {})
   end,
 }
